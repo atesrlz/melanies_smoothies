c0e5ec6f6b7d8a225162b946ca2dd078d97905cd
Attila Bognár <120170381+atesrlz@users.noreply.github.com>
2026-08-06 06:16:49 +0200

    Refactor API request for watermelon fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,7 @@ if ingredients_list:
     
         st.success('Your Smoothie is ordered!', icon="✅")
 
-import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+import requests
+url = "https://my.smoothiefroot.com/api/fruit/watermelon"
+smoothiefroot_response = requests.get(url)
 st.text(smoothiefroot_response)
