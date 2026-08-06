a087b47251b36526e9b3c0d2296d959b1aa36469
Attila Bognár <120170381+atesrlz@users.noreply.github.com>
2026-08-06 06:43:45 +0200

    Modify dataframe selection and display settings
    Updated the dataframe selection to include 'SEARCH_ON' column and adjusted the dataframe display width.

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,9 @@ st.write("The name on your Smoothie will be", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
@@ -34,7 +35,7 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get(url + fruit_chosen)
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width='stretch')
 
     # st.write(ingredients_string)
 
